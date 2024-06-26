5f4e2b7cccda1593f65715f1e458483e07bd31d7
makzs <erikmakiak@gmail.com>
2024-06-26 10:13:41 -0300

    consultar clientes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import LivroListar from './Components/Pages/Livro/Livro-Consultar';
+import ClienteListar from './Components/Pages/Cliente/Cliente-Consultar';
 
 function App() {
   return (
     <div>
       <h1>Biblioteca</h1>
       <LivroListar></LivroListar>
+      <hr></hr>
+      <ClienteListar></ClienteListar>
     </div>
   );
 }
